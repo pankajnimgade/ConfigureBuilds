00ec7237838ff11e2cc0f497de119d721cd8793f
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2018-04-16 21:58:21 -0500

    added guidlines

--- app/src/main/java/configure/test/configurebuilds/activities/custom101/view/BarChart.java
+++ app/src/main/java/configure/test/configurebuilds/activities/custom101/view/BarChart.java
@@ -2,6 +2,8 @@ package configure.test.configurebuilds.activities.custom101.view;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.util.Log;
@@ -12,8 +14,19 @@ public class BarChart extends View {
     private static final String TAG = BarChart.class.getSimpleName();
     private int maxHeight = getHeight();
     private int maxWidth = getWidth();
-    private float mPadding = 5f;
-    
+    private float mPadding = 15f;
+    private static Paint mAxisBlackPaint;
+    private static Paint mGuilLineBlackPaint;
+
+    static {
+        mAxisBlackPaint = new Paint(Color.BLACK);
+        mAxisBlackPaint.setFakeBoldText(true);
+        mAxisBlackPaint.setStrokeWidth(3f);
+
+        mGuilLineBlackPaint = new Paint(Color.BLACK);
+        mGuilLineBlackPaint.setStrokeWidth(1f);
+    }
+
     public BarChart(Context context) {
         super(context);
     }
@@ -30,6 +43,31 @@ public class BarChart extends View {
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         Log.d(TAG, "onDraw: ...");
-        
+        maxHeight = getHeight();
+        maxWidth = getWidth();
+        Log.d(TAG, "onDraw: maxHeight: " + getHeight());
+        Log.d(TAG, "onDraw: maxWidth: " + getWidth());
+        final float gridTop = mPadding;
+        final float gridRight = maxWidth - mPadding;
+        final float gridBottom = maxHeight - mPadding;
+        final float gridLeft = mPadding;
+        Log.d(TAG, "onDraw: gridTop: " + gridTop);
+        Log.d(TAG, "onDraw: gridRight: " + gridRight);
+        Log.d(TAG, "onDraw: gridBottom: " + gridBottom);
+        Log.d(TAG, "onDraw: gridLeft: " + gridLeft);
+
+        //draw charts x-axis
+        canvas.drawLine(gridLeft, gridBottom, gridRight, gridBottom, mAxisBlackPaint);
+        //draw charts y-axis
+        canvas.drawLine(gridLeft, gridTop, gridLeft, gridBottom, mAxisBlackPaint);
+
+        //draw guideLines
+        float guideLineWidth = (gridBottom - gridTop) / 10f;
+        for (int i = 0; i < 10; i++) {
+            float yValue = gridTop + (i* guideLineWidth);
+            canvas.drawLine(gridLeft, yValue, gridRight, yValue, mGuilLineBlackPaint);
+        }
+
+
     }
 }
