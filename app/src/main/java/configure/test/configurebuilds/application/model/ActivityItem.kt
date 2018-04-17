8e8ac7bbab24cedc3f57e6ffe31c2e9a2f94298b
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2018-04-16 20:42:32 -0500

    updated item decorator

--- app/src/main/java/configure/test/configurebuilds/application/model/ActivityItem.kt
+++ app/src/main/java/configure/test/configurebuilds/application/model/ActivityItem.kt
@@ -3,6 +3,7 @@ package configure.test.configurebuilds.application.model
 import android.app.Activity
 import configure.test.configurebuilds.activities.ExampleActivity
 import configure.test.configurebuilds.activities.decorator_one.DecoratorOneActivity
+import configure.test.configurebuilds.activities.decorator_two.DecoratorTwoActivity
 import configure.test.configurebuilds.activities.dock.list.ListActivity
 import configure.test.configurebuilds.activities.dock.list.TestDockTileActivity
 import configure.test.configurebuilds.activities.edittext.CustomEditTextActivity
@@ -21,6 +22,7 @@ class ActivityItem(val activityClass: Class<out Activity>, val activityName: Str
             list.add(ActivityItem(ListActivity::class.java, "List Item"))
             list.add(ActivityItem(TestDockTileActivity::class.java, "Test Dock tile"))
             list.add(ActivityItem(DecoratorOneActivity::class.java, "Decorator One"))
+            list.add(ActivityItem(DecoratorTwoActivity::class.java, "Decorator Two"))
             return list
         }
     }
