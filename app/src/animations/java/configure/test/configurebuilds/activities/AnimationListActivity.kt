fcd14ee618c736657184a9773b2222862b1dae3e
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2018-07-29 15:59:48 -0500

    added scale animation

--- app/src/animations/java/configure/test/configurebuilds/activities/AnimationListActivity.kt
+++ app/src/animations/java/configure/test/configurebuilds/activities/AnimationListActivity.kt
@@ -32,6 +32,7 @@ import configure.test.configurebuilds.activities.test101.Animation101Activity
 import configure.test.configurebuilds.activities.test102.Animation102Activity
 import configure.test.configurebuilds.activities.test103.Animation103Activity
 import configure.test.configurebuilds.activities.test104.Animation104Activity
+import configure.test.configurebuilds.activities.test105.Animation105Activity
 import configure.test.configurebuilds.application.model.ActivityItem
 import kotlinx.android.synthetic.animations.activity_animation_list.*
 
@@ -62,6 +63,7 @@ class AnimationListActivity : AppCompatActivity() {
             list.add(ActivityItem(Animation102Activity::class.java, "Animation 102"))
             list.add(ActivityItem(Animation103Activity::class.java, "Animation 103"))
             list.add(ActivityItem(Animation104Activity::class.java, "Animation 104"))
+            list.add(ActivityItem(Animation105Activity::class.java, "Animation 105"))
         }
         recyclerView.adapter = AnimationAdapter(this, list)
 
