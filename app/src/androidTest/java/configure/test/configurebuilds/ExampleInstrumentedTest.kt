cec39f146dcfcf2ddd5f78e5bfd8af4ae970e4ec
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2018-07-30 23:13:13 -0500

    instrumentation is working

--- app/src/androidTest/java/configure/test/configurebuilds/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/configure/test/configurebuilds/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
-        assertEquals("configure.test.configurebuilds.dev.debug", appContext.packageName)
+        assertEquals("configure.test.configurebuilds.demotest.debug", appContext.packageName)
     }
 }
