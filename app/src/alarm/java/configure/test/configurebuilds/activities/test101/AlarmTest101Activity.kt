98879e93a6f5833ada3bd4748e6b4b791d37f1d1
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2019-04-11 22:15:52 -0500

    set the first alarm example

--- app/src/alarm/java/configure/test/configurebuilds/activities/test101/AlarmTest101Activity.kt
+++ app/src/alarm/java/configure/test/configurebuilds/activities/test101/AlarmTest101Activity.kt
@@ -1,11 +1,22 @@
 package configure.test.configurebuilds.activities.test101
 
+import android.app.AlarmManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.os.SystemClock
 import android.support.v7.app.AppCompatActivity
 import configure.test.configurebuilds.R
 import kotlinx.android.synthetic.alarm.activity_alarm_test101.*
 
+private val TAG = "Test101"
+
 class AlarmTest101Activity : AppCompatActivity() {
+
+    private lateinit var mPendingIntent: PendingIntent
+    private var alarmMgr: AlarmManager? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -13,6 +24,27 @@ class AlarmTest101Activity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
+        initializePendingIntent()
 
+        initializeUi()
+    }
+
+    private fun initializePendingIntent() {
+        val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/"))
+        mPendingIntent = PendingIntent.getActivity(this,
+                1, intent, PendingIntent.FLAG_UPDATE_CURRENT)
+    }
+
+    private fun initializeUi() {
+
+        btn_1_alarm.setOnClickListener {
+            alarmMgr = baseContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
+            alarmMgr?.setInexactRepeating(
+                    AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                    SystemClock.elapsedRealtime() + (1 * 60 * 1000),
+                    AlarmManager.INTERVAL_DAY,
+                    mPendingIntent
+            )
+        }
     }
 }
