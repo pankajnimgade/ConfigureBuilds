758735189a10fc932b4c0cd41c8e927ee6d95613
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2019-04-25 23:31:12 -0500

    implemented repeating alarm for every minute

--- app/src/alarm/java/configure/test/configurebuilds/activities/test104/AlarmTest104Activity.kt
+++ app/src/alarm/java/configure/test/configurebuilds/activities/test104/AlarmTest104Activity.kt
@@ -1,25 +1,99 @@
 package configure.test.configurebuilds.activities.test104
 
+import android.app.*
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.support.v4.app.NotificationCompat
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
 import android.view.View
 import configure.test.configurebuilds.R
 import kotlinx.android.synthetic.alarm.activity_alarm_test104.*
+import java.util.*
+
 
 private val TAG = "AlarmTest104"
 
 class AlarmTest104Activity : AppCompatActivity() {
+
+    companion object {
+
+        const val CHANNEL_ID = "CHANNEL_ID"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_alarm_test104)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+
+        initializeNotificationChannel()
     }
+
 
     fun repeatingAlarm(view: View) {
 
         Log.d(TAG, ": ")
+        scheduleNotification()
+    }
+
+    private fun scheduleNotification() {
+
+        Log.d(TAG, ": scheduleNotification()")
+
+        val notificationIntent = Intent(this, NotificationPublisher::class.java)
+
+        val pendingIntent = PendingIntent.getBroadcast(
+                applicationContext, PendingIntent.FLAG_UPDATE_CURRENT,
+                notificationIntent, PendingIntent.FLAG_UPDATE_CURRENT)
+
+        val alarmManager = getSystemService(Context.ALARM_SERVICE) as AlarmManager
+        val instance = Calendar.getInstance().apply {
+            set(Calendar.HOUR_OF_DAY, 23)
+            set(Calendar.MINUTE, 4)
+        }
+
+        alarmManager.setInexactRepeating(
+                AlarmManager.RTC_WAKEUP,
+                instance.timeInMillis,
+                (1 * 60 * 1000),
+                pendingIntent)
+
+
+    }
+
+    private fun getNotification(context: Context): Notification {
+
+        Log.d(TAG, ": getNotification()")
+
+        var builder = NotificationCompat.Builder(context, AlarmTest104Activity.CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.btn_plus)
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        return builder.build()
+    }
+
+
+    private fun initializeNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            // Create the NotificationChannel
+            val name = "CHANNEL NAME"
+            val descriptionText = "CHANNEL DESCRIPTION"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val mChannel = NotificationChannel(CHANNEL_ID, name, importance)
+            mChannel.description = descriptionText
+            // Register the channel with the system; you can't change the importance
+            // or other notification behaviors after this
+            val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(mChannel)
+        }
     }
 }
---
+++ app/src/alarm/java/configure/test/configurebuilds/activities/test104/NotificationPublisher.kt
@@ -0,0 +1,46 @@
+package configure.test.configurebuilds.activities.test104
+
+import android.app.Notification
+import android.app.NotificationManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.NotificationCompat
+import android.util.Log
+import java.util.*
+
+
+/**
+ * Created by Pankaj Nimgade on 4/25/2019.
+ */
+
+private const val TAG = "NotificationPub"
+
+class NotificationPublisher : BroadcastReceiver() {
+
+    var NOTIFICATION_ID = "notification-id"
+    var NOTIFICATION = "notification"
+
+    override fun onReceive(context: Context, intent: Intent) {
+
+        Log.d(TAG, ": onReceive()")
+        val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify((Random().nextInt(100)), getNotification(context))
+
+    }
+
+    private fun getNotification(context: Context): Notification {
+
+        Log.d(TAG, ": getNotification()")
+
+        var builder = NotificationCompat.Builder(context, AlarmTest104Activity.CHANNEL_ID)
+                .setSmallIcon(android.R.drawable.btn_plus)
+                .setContentTitle("My notification")
+                .setContentText("Much longer text that cannot fit one line...")
+                .setStyle(NotificationCompat.BigTextStyle()
+                        .bigText("Much longer text that cannot fit one line..."))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        return builder.build()
+    }
+}
