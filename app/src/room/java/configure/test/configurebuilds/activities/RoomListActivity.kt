0ba9823950cf552f4a6259d0c38980f5bf923490
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2019-04-27 22:34:23 -0500

    added initial entry point activity for Room list

---
+++ app/src/room/java/configure/test/configurebuilds/activities/RoomListActivity.kt
@@ -0,0 +1,24 @@
+package configure.test.configurebuilds.activities
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+import configure.test.configurebuilds.R
+
+import kotlinx.android.synthetic.room.activity_room_list.*
+
+class RoomListActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_room_list)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+}
