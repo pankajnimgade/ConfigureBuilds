b36b242d893b7d75f48134cc56f8ab6a4c29f50f
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2019-05-11 18:52:10 -0500

    created theme for the activity which would change every view in that activity

--- app/src/userInterface/java/configure/test/configurebuilds/activities/test102/Test102ThemeActivity.kt
+++ app/src/userInterface/java/configure/test/configurebuilds/activities/test102/Test102ThemeActivity.kt
@@ -1,15 +1,15 @@
-package configure.test.configurebuilds.activities.test101
+package configure.test.configurebuilds.activities.test102
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import configure.test.configurebuilds.R
-import kotlinx.android.synthetic.userInterface.activity_test101_style.*
+import kotlinx.android.synthetic.userInterface.activity_test102_theme.*
 
-class Test101StyleActivity : AppCompatActivity() {
+class Test102ThemeActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_test101_style)
+        setContentView(R.layout.activity_test102_theme)
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
