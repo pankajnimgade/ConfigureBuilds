e74a9747e7df753d5edfd9a3e1833c71aa59b764
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2019-05-11 19:12:10 -0500

    compleated reminaing migration to androidX

--- app/src/recyclerView/java/configure/test/configurebuilds/utils/RecyclerViewFastScroller.java
+++ app/src/recyclerView/java/configure/test/configurebuilds/utils/RecyclerViewFastScroller.java
@@ -8,7 +8,7 @@ import android.content.Context;
 import android.support.annotation.IdRes;
 import android.support.annotation.LayoutRes;
 import android.support.annotation.NonNull;
-import android.support.v4.view.ViewCompat;
+import androidx.core.view.ViewCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.text.TextUtils;
