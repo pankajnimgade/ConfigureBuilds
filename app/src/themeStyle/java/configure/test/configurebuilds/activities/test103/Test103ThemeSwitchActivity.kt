851e406d8105ed6e9247b9d9573657a49a733270
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2019-05-26 16:11:33 -0500

    added theme shared preference to keep tack of which theme to be implemented as per user choice

--- app/src/themeStyle/java/configure/test/configurebuilds/activities/test103/Test103ThemeSwitchActivity.kt
+++ app/src/themeStyle/java/configure/test/configurebuilds/activities/test103/Test103ThemeSwitchActivity.kt
@@ -6,14 +6,13 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.app.AppCompatDelegate
 import configure.test.configurebuilds.R
 import kotlinx.android.synthetic.themeStyle.activity_test103_theme_switch.*
 
 class Test103ThemeSwitchActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+        delegate.localNightMode = ThemePreference.getInstance(application).getThemeMode()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test103_theme_switch)
         setSupportActionBar(toolbar)
@@ -30,14 +29,20 @@ class Test103ThemeSwitchActivity : AppCompatActivity() {
         return when (item?.itemId) {
             R.id.theme_light -> {
                 Toast.makeText(applicationContext, "Light", Toast.LENGTH_SHORT).show()
+                ThemePreference.getInstance(application).saveThemeMode(ThemeMode.DAY_LIGHT)
+                recreate()
                 true
             }
             R.id.theme_dark -> {
                 Toast.makeText(applicationContext, "Dark", Toast.LENGTH_SHORT).show()
+                ThemePreference.getInstance(application).saveThemeMode(ThemeMode.NIGHT_DARK)
+                recreate()
                 true
             }
             R.id.theme_system -> {
+                ThemePreference.getInstance(application).saveThemeMode(ThemeMode.SYSTEM_AUTO)
                 Toast.makeText(applicationContext, "System", Toast.LENGTH_SHORT).show()
+                recreate()
                 true
             }
             else -> super.onOptionsItemSelected(item)
