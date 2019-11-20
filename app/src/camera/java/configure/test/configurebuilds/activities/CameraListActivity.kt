8638103f0af51a62a59ce1d025e7e64dfa7f9d35
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2019-11-19 20:23:46 -0600

    refactored code base to handle recent updates from the google

--- app/src/camera/java/configure/test/configurebuilds/activities/CameraListActivity.kt
+++ app/src/camera/java/configure/test/configurebuilds/activities/CameraListActivity.kt
@@ -1,8 +1,9 @@
 package configure.test.configurebuilds.activities
 
 import android.os.Bundle
-import android.support.design.widget.Snackbar
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.snackbar.Snackbar
 import configure.test.configurebuilds.R
 import kotlinx.android.synthetic.camera.activity_camera_list.*
 
@@ -12,7 +13,7 @@ class CameraListActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera_list)
-        setSupportActionBar(toolbar)
+        setSupportActionBar(toolbar as Toolbar?)
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
