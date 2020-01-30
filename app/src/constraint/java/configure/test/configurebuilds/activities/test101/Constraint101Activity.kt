74f904e983bd8d9b9e651356a9b01986a05b42d5
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2020-01-29 21:57:56 -0600

    added another activity

---
+++ app/src/constraint/java/configure/test/configurebuilds/activities/test101/Constraint101Activity.kt
@@ -0,0 +1,13 @@
+package configure.test.configurebuilds.activities.test101
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import configure.test.configurebuilds.R
+
+class Constraint101Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_constraint101)
+    }
+}
