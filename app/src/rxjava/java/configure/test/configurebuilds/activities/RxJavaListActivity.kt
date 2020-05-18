ae54520a60824445444448eb84040ff9e4d637ae
Pankaj Nimgade <pankaj.nimgade@gmail.com>
2020-05-18 13:15:42 -0500

    added recycler view for the rxJava list

--- app/src/rxjava/java/configure/test/configurebuilds/activities/RxJavaListActivity.kt
+++ app/src/rxjava/java/configure/test/configurebuilds/activities/RxJavaListActivity.kt
@@ -1,23 +1,70 @@
 package configure.test.configurebuilds.activities
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
 import configure.test.configurebuilds.R
+import configure.test.configurebuilds.activities.test101.RxJava101Activity
+import configure.test.configurebuilds.application.model.ActivityItem
 import kotlinx.android.synthetic.rxjava.activity_rx_java_list.*
 
 
 class RxJavaListActivity : AppCompatActivity() {
 
+    private val list: MutableList<ActivityItem> = mutableListOf()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_rx_java_list)
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        fab.setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+        initializeUi()
     }
 
+    private fun initializeUi() {
+
+        if (list.isEmpty()) {
+            list.add(ActivityItem(RxJava101Activity::class.java, "RxJava 101"))
+        }
+        recycler_view.adapter = RxJavaAdapter(list)
+    }
+
+    class RxJavaAdapter(val list: List<ActivityItem>) :
+            RecyclerView.Adapter<RxJavaAdapter.ViewHolder>() {
+
+        class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+            val rootView: CardView = itemView.findViewById(R.id.single_activity_item_root_CardView)
+            val activityTitle: TextView = itemView.findViewById(R.id.single_activity_item_name_textView)
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val layoutInflater = LayoutInflater.from(parent.context)
+            val inflate = layoutInflater.inflate(R.layout.single_activity_item, parent, false)
+            val viewHolder = ViewHolder(inflate)
+            viewHolder.itemView.setOnClickListener {
+                val activityClass = list[viewHolder.bindingAdapterPosition]
+                parent.context?.let {
+                    it.startActivity(Intent(it, activityClass.activityClass))
+                }
+            }
+
+            return viewHolder
+        }
+
+        override fun getItemCount(): Int {
+            return list.size
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val activityItem = list[position]
+            holder.activityTitle.text = activityItem.activityName
+        }
+    }
 }
---
+++ app/src/rxjava/java/configure/test/configurebuilds/activities/test101/RxJava101Activity.kt
@@ -0,0 +1,13 @@
+package configure.test.configurebuilds.activities.test101
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import configure.test.configurebuilds.R
+
+class RxJava101Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_rx_java101)
+    }
+}
